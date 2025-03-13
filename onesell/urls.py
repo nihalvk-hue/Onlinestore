a488f2ac57a6ac0edcfec946ec11c32cf5ca16eb
nihalvk-hue <nihalvk01@gmail.com>
2025-03-13 11:10:26 +0000

    Added sitemap integration

--- onesell/urls.py
+++ onesell/urls.py
@@ -9,19 +9,19 @@ from products.sitemaps import StaticViewSitemap, ProductSitemap  # Import your s
 sitemaps = {
     'static': StaticViewSitemap,
         'products': ProductSitemap,
-        }
+}
 
-        urlpatterns = [
-            path('admin/', admin.site.urls),
-                path('', include('products.urls')),
-                    path('cart/', include('cart.urls')),
-                        path('orders/', include('orders.urls')),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('products.urls')),
+    path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls')),
 
-                            # Sitemap URL
-                                path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-                                ]
+    # Sitemap URL
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+]
 
-                                # Serve static and media files in development
-                                if settings.DEBUG:
-                                    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-                                        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Serve static and media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
