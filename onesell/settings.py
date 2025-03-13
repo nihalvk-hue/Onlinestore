5993f978bad7bb8ebf488b08e6b1d990ee263174
nihalvk-hue <nihalvk01@gmail.com>
2025-03-13 10:54:37 +0000

    Added sitemap integration

--- onesell/settings.py
+++ onesell/settings.py
@@ -61,7 +61,8 @@ INSTALLED_APPS = [
     "products",
     "cart",
     "orders",
-    "onesell"
+    "onesell",
+    "django.contrib.sitemaps",
 ]
 
 # Middleware
---
+++ onesell/sitemaps.py
@@ -0,0 +1,23 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from .models import Product  # Assuming you have a Product model
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+        changefreq = 'weekly'
+
+            def items(self):
+                    return ['product_list', 'homepage']  # Add your important views
+
+                        def location(self, item):
+                                return reverse(item)
+
+                                class ProductSitemap(Sitemap):
+                                    priority = 0.8
+                                        changefreq = 'daily'
+
+                                            def items(self):
+                                                    return Product.objects.all()
+
+                                                        def lastmod(self, obj):
+                                                                return obj.updated_at  # Make sure your Product model has an "updated_at" field
