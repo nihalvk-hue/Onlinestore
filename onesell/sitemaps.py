9934c0e75bb0767d73d2e8dc4b65090011d2c8d9
nihalvk-hue <nihalvk01@gmail.com>
2025-03-13 11:21:22 +0000

    Added sitemap for SEO

--- onesell/sitemaps.py
+++ onesell/sitemaps.py
@@ -4,20 +4,20 @@ from .models import Product  # Assuming you have a Product model
 
 class StaticViewSitemap(Sitemap):
     priority = 0.5
-        changefreq = 'weekly'
+    changefreq = 'weekly'
 
-            def items(self):
-                    return ['product_list', 'homepage']  # Add your important views
+    def items(self):
+        return ['product_list', 'homepage']  # Add your important views
 
-                        def location(self, item):
-                                return reverse(item)
+    def location(self, item):
+        return reverse(item)
 
-                                class ProductSitemap(Sitemap):
-                                    priority = 0.8
-                                        changefreq = 'daily'
+class ProductSitemap(Sitemap):
+    priority = 0.8
+    changefreq = 'daily'
 
-                                            def items(self):
-                                                    return Product.objects.all()
+    def items(self):
+        return Product.objects.all()
 
-                                                        def lastmod(self, obj):
-                                                                return obj.updated_at  # Make sure your Product model has an "updated_at" field
+    def lastmod(self, obj):
+        return obj.updated_at  # Make sure your Product model has an "updated_at" field 
